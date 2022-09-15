81c518d38a5cdb48645f010a76184290deec7f43
“meetkelly” <kelechishalom@gmail.com>
2022-09-15 02:42:48 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints _putchar followed by a new line 
- * Return: 0 on success 
+* main - prints _putchar followed by a new line 
+* Return: 0 on success 
  */
 
 int main(void)
